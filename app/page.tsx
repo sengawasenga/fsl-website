cff18c02565b220abf7369116115efd16375996f
sengawasenga <14marcel.senga@gmail.com>
2026-03-03 00:24:57 +0100

    Update home page text content for charming welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import { Button } from "@heroui/react";
 
 const HomePage = () => {
   return (
-    <div>
-      <Button variant="primary">My Button</Button>
+    <div className="min-h-screen flex flex-col justify-center items-center gap-4">
+      <h1 className="text-7xl">Mbote na bino!</h1>
+      <p className="text-2xl">Esengo mingi ko mona bino !</p>
     </div>
   );
 };
