83a69432784fb7aa337e432f252155caa5008992
sengawasenga <14marcel.senga@gmail.com>
2026-03-03 20:29:52 +0100

    Add the gallery page

---
+++ app/(website)/galerie/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GalleryPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center gap-4">
+      <h1 className="text-8xl font-ashigea">Galerie</h1>
+      <p className="text-2xl">Esengo mingi ko mona bino !</p>
+    </div>
+  );
+};
+
+export default GalleryPage;
